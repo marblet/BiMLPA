eb1a5b20dbfa56f09bc277839968cd3c343cdf45
Hibiki Taguchi <55818491+hbkt@users.noreply.github.com>
2019-11-05 11:30:12 +0900

    change variable name of graph
    Variable name of graph is unified to G (not g)

--- BiMLPA/test/test_bimlpa.py
+++ BiMLPA/test/test_bimlpa.py
@@ -6,30 +6,30 @@ class BiMLPATestCase(unittest.TestCase):
 
     def test_bimlpa_SqrtDeg(self):
 
-        g = generate_network('southernwomen.net')
-        bimlpa = BiMLPA_SqrtDeg(g, 0.3, 7)
+        G = generate_network('southernwomen.net')
+        bimlpa = BiMLPA_SqrtDeg(G, 0.3, 7)
         bimlpa.start()
-        relabeling(g)
-        top, bottom = output_community(g)
+        relabeling(G)
+        top, bottom = output_community(G)
         self.assertIsInstance(top, list)
         self.assertIsInstance(bottom, list)
 
     def test_bimlpa(self):
 
-        g = generate_network_withName('southernwomen.net')
-        bimlpa = BiMLPA(g, 0.3, 7)
+        G = generate_network_withName('southernwomen.net')
+        bimlpa = BiMLPA(G, 0.3, 7)
         bimlpa.start()
-        relabeling(g)
-        top, bottom = output_community(g)
+        relabeling(G)
+        top, bottom = output_community(G)
         self.assertIsInstance(top, list)
         self.assertIsInstance(bottom, list)
 
     def test_bimlpa_BiMLPA_EdgeProb(self):
-        g = generate_network('southernwomen.net')
-        bimlpa = BiMLPA_EdgeProb(g, 0.3, 7)
+        G = generate_network('southernwomen.net')
+        bimlpa = BiMLPA_EdgeProb(G, 0.3, 7)
         bimlpa.start()
-        relabeling(g)
-        top, bottom = output_community(g)
+        relabeling(G)
+        top, bottom = output_community(G)
         self.assertIsInstance(top, list)
         self.assertIsInstance(bottom, list)
 
